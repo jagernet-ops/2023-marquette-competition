649ced456e789756fc57213548d90ec704ddc86d
jagernet-ops <mshalewski@outlook.com>
2023-03-30 21:41:49 -0500

    removed packages

--- PRACTICE/MARCH/SKELETONS/GoOn.java
+++ PRACTICE/MARCH/SKELETONS/GoOn.java
@@ -1,21 +1,19 @@
-package PRACTICE;
 import java.util.Scanner;
 
 public class GoOn {
     public static int indexOf(String[] userList, char userLetter){
         int result = -1;
 
-        for(int i = 0; i < userList.length; i++){
-            for(int j = 0; j < userList[i].length(); j++){
-                if(userList[i].toLowerCase().charAt(0) == userLetter){
-                    result = i;
-                }
-            }
-        }
+        // Write some code here
 
         return result;
     }
     public static void main(String[] args){
+        /*
+        * It is unnecessary to edit the "main" method of each problem's provided code
+        * skeleton. The main method is written for you in order to help you conform to
+        * input and output formatting requirements.
+        */
         Scanner scanner = new Scanner(System.in);
         int numberOfCases = Integer.parseInt(scanner.nextLine());
         for(int i = 0; i < numberOfCases; i++){
