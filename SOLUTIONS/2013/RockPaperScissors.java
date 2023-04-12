43c4c5fbe755466906214ec7bd5255cd0321bf05
Michael J. Shalewski <mshalewski@outlook.com>
2023-04-12 15:30:20 +0000

    finished RockPaperScissors problem

--- SOLUTIONS/2013/RockPaperScissors.java
+++ SOLUTIONS/2013/RockPaperScissors.java
@@ -1,36 +1,39 @@
 import java.util.Scanner;
 
-public class Contains {
+public class RockPaperScissors {
 
     public static int[] contest(String userThrow){
-        int[] results = new int[2];
+        int[] results = new int[3];
         char lettermanChar = userThrow.charAt(0);
-        switch (indexedChar) {
+        switch (lettermanChar) {
         case 'R':
             if(userThrow.charAt(1) == 'S'){
                 results[0]++;
             }else if(userThrow.charAt(1) == 'P'){
-
+                results[1]++;
             }else{
-                 
+                results[0]++;
+                results[1]++;
             }
         break;
         case 'S':
-            if(userThrow.charAt(1) == 'S'){
-
+            if(userThrow.charAt(1) == 'R'){
+                results[1]++;
             }else if(userThrow.charAt(1) == 'P'){
-
+                results[0]++;
             }else{
-                
+                results[0]++;
+                results[1]++;
             }
         break;
         case 'P':
             if(userThrow.charAt(1) == 'S'){
-
-            }else if(userThrow.charAt(1) == 'P'){
-
+                results[1]++;
+            }else if(userThrow.charAt(1) == 'R'){
+                results[0]++;
             }else{
-                
+                results[0]++;
+                results[1]++;
             }
         break;
     
@@ -38,7 +41,7 @@ public class Contains {
             break;
         }
 
-        return winner;
+        return results;
     }
 
     public static void main(String[] args){
@@ -49,20 +52,28 @@ public class Contains {
         */
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        int[] winner = {};
+        int[] winner = new int[3];
         int numberOfCases = Integer.parseInt(scanner.nextLine());
         for(int i = 0; i < numberOfCases; i++){
-            do{
+            while(true){
                 userInput = scanner.nextLine();
-                winner = contest(userInput);
-            }while(userInput.toLowerCase() != "end");
+                if(userInput.toLowerCase().equals("end")){
+                    break;
+                }else{
+                    int [] results = contest(userInput);
+                    for(int j = 0; j < results.length; j++){
+                        winner[j] += results[j];
+                    }
+                }
+            }
             if(winner[0] > winner[1]){
-                System.out.println("Regis wins");
-            }else if(winner[1] > winner[0]){
                 System.out.println("David wins");
+            }else if(winner[1] > winner[0]){
+                System.out.println("Regis wins");
             }else{
                 System.out.println("Tie");
             }
         }
+        scanner.close();
     }
 }
