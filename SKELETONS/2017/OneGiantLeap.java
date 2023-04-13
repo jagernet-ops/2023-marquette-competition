bacebc042f039242e3a55905df7beb77d01e3be4
python-42 <herrmann.jake@protonmail.com>
2023-04-13 08:43:38 -0500

    Fixed OneGiantLeap skeleton

--- SKELETONS/2017/OneGiantLeap.java
+++ SKELETONS/2017/OneGiantLeap.java
@@ -7,17 +7,21 @@ public class OneGiantLeap {
 	// The main method handles standard input and output
 	// You should not change this method
 	public static void main(String [] args){
-		Scanner scanner = new Scanner(System.in);
-		int t= scanner.nextInt();
-		for(int i=0;i solution = findLeapYears(startingYear, endingYear);
-			Integer solution2[] = new Integer[solution.size()];
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for(int i = 0; i < num; i++){
+			int a = scanner.nextInt();
+        	int b = scanner.nextInt();
+			ArrayList<Integer> solution = findLeapYears(a, b);
+            Integer solution2[] = new Integer[solution.size()];
 			solution2 = solution.toArray(solution2);
 			//to print your array
 			System.out.println(Arrays.toString(solution2));
-		}
-	}
-	public static ArrayList findLeapYears(int startingYear, int endingYear){
-		ArrayList  leapYears = new  ArrayList();
+        }
+        scanner.close();
+    }
+	public static ArrayList<Integer> findLeapYears(int startingYear, int endingYear){
+		ArrayList<Integer>  leapYears = new  ArrayList<Integer>();
 
 		//TODO: Write your solution in the body of this method
 		return leapYears;
